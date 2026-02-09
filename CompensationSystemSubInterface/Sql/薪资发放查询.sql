e0b69eed8bf3e803ea14528d60f4557eff4ed664
lijinlong <lijinlong@pep.com.cn>
2026-02-09 14:53:31 +0800

    调整薪资发放信息查询语句

--- CompensationSystemSubInterface/Sql/薪资发放查询.sql
+++ CompensationSystemSubInterface/Sql/薪资发放查询.sql
@@ -3,8 +3,9 @@
 	h.EmployeeId AS '员工ID',
 	h.SalaryMonth AS '薪资月份' 
 FROM
-	ZX_SalaryHeaders h 
+	ZX_SalaryHeaders h
+	JOIN ZX_SalaryAuditBatches ab ON h.BatchId = ab.BatchId 
 WHERE
-	h.BatchId IN ( 3, 5 ) 
+	ab.OverallStatus IN ( 3, 5 ) 
 ORDER BY
 	h.DisplayOrder ASC
